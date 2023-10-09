2e87ec7725ceb87357f504d1bc4a515be65e608d
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2023-10-09 09:55:00 +0200

    Create mongodb connection

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+require("dotenv").config()
+const mongoose = require("mongoose")
+const User = require("./models/user")
 const app = express();
 
 // view engine setup
@@ -16,8 +18,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const mongoDB = process.env.MONGOURL
+mongoose.connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
+const apiRouter = require("./routes/api")
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
