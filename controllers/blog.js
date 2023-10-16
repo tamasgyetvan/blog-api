198e4c921159d50db92c6120cf3c624eb4e26bf3
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2023-10-16 20:24:04 +0200

    Create createPost controller

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,13 +1,47 @@
+const { validationResult } = require("express-validator")
 const Post = require("../models/post")
+const asyncHandler = require("express-async-handler")
 
 
 exports.queryPosts = async(req, res, next) => {
-    res.json("Blog get posts from DB request")
+    const posts = Post.find({}, "title").exec()
+    res.json(posts)
 }
 
-exports.createPost = async(req, res, next) => {
-    res.json("Blog create post request")
-}
+exports.createPost = [
+    body("username")
+        .isLength({max: 50})
+        .trim()
+        .escape(),
+    body("text")
+        .isLength({max: 50})
+        .trim()
+        .escape(),
+    body("user")
+        .trim()
+        .escape(),
+
+    asyncHandler(async(req, res, next) => {
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty()) {
+            res.json(errors.array())
+            return;
+        } else {
+            const newPost = new Post({
+                title: req.body.title,
+                text: req.body.content,
+                author: req.body.user
+            })
+
+            await newPost.save()
+            res.json("Post created")
+        }
+        
+        
+    })
+
+]
 
 exports.deletePost = async(req, res, next) => {
     res.json("Blog delete post request")
