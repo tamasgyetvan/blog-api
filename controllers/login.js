4aa72dacfc3d8614682fd274ebb584327a92e8ba
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2023-10-19 09:57:06 +0200

    Code cleanup

--- controllers/login.js
+++ controllers/login.js
@@ -25,49 +25,16 @@ const bcrypt = require("bcryptjs")
             const user = await User.findOne({username: req.body.username})
             
             if (user && (await bcrypt.compare(req.body.password, user.password)) ) {
-            const token = jwt.sign({id: user._id}, process.env.SECRET, {expiresIn: "5m"})                       
+            const token = jwt.sign({id: user._id}, process.env.SECRET, {expiresIn: "15m"})                       
             res.status(200).json({
                 user: user._id,
                 token,
                 errorMessage: null
-            })
-        } else if (!user) {
-            res.status(400).json({
+            })} else if (!user) {
+                res.status(400).json({
                 token: null,
                 errorMessage: "User not found"})
-        }
-        } catch(error) {
-            res.json(error)
-        }
-    }
-    /* asyncHandler(async(req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            res.status(400).json(errors)
-        } 
-
-        const user = await User.findOne({username: req.body.username})      
-        if (user && (await bcrypt.compare(req.body.password, user.password)) ) {
-            jwt.sign({id: user._id}),
-            process.env.SECRET,
-            {
-                expiresIn: "5m"
-            }
-            user.token = token;
-            user.password = undefined;
-
-            //send token to cookie
-            const options = {
-                expires: new Date(Date.now() + 0.00347222 * 24 * 60 * 60 * 1000),
-                httpOnly: true
-            }
-            res.status(200).cookie("token", token, options).json({
-                token,
-                user
-            }
-
-            )
-
-        }
-    }) */
- ]
+                }} catch(error) {
+                     res.json(error)
+                }
+        }]
