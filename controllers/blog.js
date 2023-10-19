19f86e3447944ef18e01eb59362b2b88af9babf0
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2023-10-19 09:54:57 +0200

    Change createPost response on success

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,14 +1,14 @@
-const { validationResult } = require("express-validator")
+const { validationResult, body } = require("express-validator")
 const Post = require("../models/post")
 const asyncHandler = require("express-async-handler")
-
+const  jwt  = require("jsonwebtoken")
 
 exports.queryPosts = async(req, res, next) => {
-    const posts = Post.find({}, "title").exec()
-    res.json(posts)
+        const posts = await Post.find({}).exec()
+        res.json(posts)
 }
 
-exports.createPost = [
+exports.createPost =  [
     body("username")
         .isLength({max: 50})
         .trim()
@@ -25,23 +25,19 @@ exports.createPost = [
         const errors = validationResult(req)
 
         if (!errors.isEmpty()) {
-            res.json(errors.array())
-            return;
+            res.json({errorMessage: "Error when creating Post"})
+            
         } else {
             const newPost = new Post({
                 title: req.body.title,
                 text: req.body.content,
                 author: req.body.user
             })
-
             await newPost.save()
-            res.json("Post created")
-        }
-        
-        
+            res.json({successMessage: "Post created"})
+        }        
     })
-
-]
+] 
 
 exports.deletePost = async(req, res, next) => {
     res.json("Blog delete post request")
