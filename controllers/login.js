ef5d7e618d3e23c9a944613f92bfade4eaacca4f
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2023-11-14 10:27:10 +0100

    Rework login controller

--- controllers/login.js
+++ controllers/login.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken")
 const User = require("../models/user")
 const { body, validationResult } = require("express-validator")
 const bcrypt = require("bcryptjs")
+const asyncHandler = require("express-async-handler")
 
  exports.login_post = [
     body("username")
@@ -13,31 +14,24 @@ const bcrypt = require("bcryptjs")
         .trim()
         .escape(),
     
-    async(req, res) => {
-        try {
-            const errors = validationResult(req);
-            if (!errors.isEmpty()) {
-                res.json({
-                        token: null,
-                        errorMessage: "Server error"})
-                return;
-            }
 
-            const user = await User.findOne({username: req.body.username})
-            
-            if (user && (await bcrypt.compare(req.body.password, user.password)) ) {
-            const token = jwt.sign({id: user._id}, process.env.SECRET, {expiresIn: "15m"})                       
-            res.json({
-                user: user._id,
-                token: token,
-                errorMessage: null
-            })} else if (!user) {
-                res.json({
-                token: null,
-                errorMessage: "Username not found!"})
+    asyncHandler((async(req, res, next) => {
+        const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                res.json({token: null, errorMessage: "Server error!"})
+            } else {
+                const user = await User.findOne({username: req.body.username})
+                if (user && (await bcrypt.compare(req.body.password, user.password)) ) {
+                    const token = jwt.sign({id: user._id}, process.env.SECRET, {expiresIn: "15m"})                       
+                    res.json({
+                        user: user._id,
+                        token: token,
+                        errorMessage: null
+                    })
                 } else {
-                    res.json({token: null, errorMessage: "Incorrect password!"})
-                }} catch(error) {
-                     res.json(error)
-                }
-        }]
+                    res.json({
+                        token: null,
+                        errorMessage: "Incorrect username or password!"})
+                } 
+            }
+    }))]
